e1a704359ef76472c517438cb6bf7161f09c4114
Tarek Labidi <63077008+TarekLabidi@users.noreply.github.com>
2024-02-12 20:23:17 +0100

    removing apikey

--- lib/data.dart
+++ lib/data.dart
@@ -4,7 +4,7 @@ class Weather {
   Future GetCurrentWeather() async {
     try {
       const apiKey =
-          '69fa17bd7ec7b060c3ccd21d5d9307b6'; // Replace this with your actual API key
+          '69fa17bd7ec7b060c3ccdb6'; // Replace this with your actual API key
       const city = 'Monastir,TN';
       final url = Uri.parse(
           'https://api.openweathermap.org/data/2.5/weather?q=$city,uk&APPID=$apiKey');
